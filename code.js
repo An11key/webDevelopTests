ca94fb6e6e04fdbf75facca948e695899b9c81f0
An11key <71543589+An11key@users.noreply.github.com>
2023-03-13 00:26:39 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,6 +1,23 @@
 
+let textElement = document.querySelector('.horizontal-swiper .swiper-wrapper');
+let n = 3;
+let s = '';
+for (let i = 1; i <= n; i++) {
+    s += `<div class = "swiper-slide"><div class = "image"><img src="/images/img${i}_horizontal.jpg"></div></div>`;
+}
+textElement.innerHTML = s;
 
-new Swiper('.swiper-container', {
+
+textElement = document.querySelector('.vertical-swiper .swiper-wrapper');
+n = 2;
+s = '';
+for (let i = 1; i <= n; i++) {
+    s += `<div class = "swiper-slide"><div class = "image"><img src="/images/img${i}_vertical.jpg"></div></div>`;
+}
+textElement.innerHTML = s;
+
+
+new Swiper('.horizontal-swiper', {
     loop: true,
 
     autoplay: {
@@ -9,4 +26,25 @@ new Swiper('.swiper-container', {
     },
 
     speed: 3000,
+
+    direction: 'horizontal',
 });
+
+new Swiper('.vertical-swiper', {
+    loop: true,
+
+    autoplay: {
+        delay: 0,
+        stopOnLastSlide: false,
+    },
+
+    speed: 3000,
+
+    direction: 'vertical',
+});
+
+
+
+
+
+
